76b1b724590411dbe579b1b438e7a7ad0e4adfd2
sidgupta333 <siddhantgupta89@gmail.com>
2024-02-22 22:32:31 +0530

    [CHORE] : Adding Elasticsearch docker instance and Recommendation service

---
+++ microservices/recommedation-service/src/main/java/com/wms/recommedationservice/RecommedationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wms.recommedationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommedationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommedationServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/recommedation-service/src/test/java/com/wms/recommedationservice/RecommedationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wms.recommedationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommedationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
