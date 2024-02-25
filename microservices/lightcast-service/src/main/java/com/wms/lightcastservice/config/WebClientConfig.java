5ba16eab7fe106948f6f7e802eb48f67a4556f6d
sidgupta333 <siddhantgupta89@gmail.com>
2024-02-25 15:32:24 +0530

    Update WebClientConfig.java

--- microservices/lightcast-service/src/main/java/com/wms/lightcastservice/config/WebClientConfig.java
+++ microservices/lightcast-service/src/main/java/com/wms/lightcastservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.wms.lightcastservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
@@ -9,7 +10,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
+    @LoadBalanced
+    public WebClient.Builder webClientBuilderForServices() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilderForExternalApi() {
         final int size = 16 * 1024 * 1024;
         final ExchangeStrategies strategies = ExchangeStrategies.builder()
                 .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
